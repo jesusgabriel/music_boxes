7ef8d3bca76f6f1ca46d3aaf597b61c411c714b0
Andy Melo <amelo@fvi.edu>
2016-09-22 18:32:04 -0400

    create music boxes

---
+++ mobile-menu/js/app.js
@@ -0,0 +1,19 @@
+//Problem: It look gross in smaller browser widths and small devices
+//Solution: To hide the text links and swap them out with a more appropriate navigation
+
+//create a select element
+
+//append items from menu as options to the select
+  //create option element
+  //options value is an href
+  //options text is the text of link
+  //append option to select
+
+//bind page changing action to the event where the select option changes
+
+
+//modify CSS so select is hidden in large screens
+//modify CSS so select is hidden in small screens
+
+
+//the page that loads should b
